597246096f0251e7725e8a709940bf9acbc62724
laurahuang106 <huangyuying2018@hotmail.com>
2023-11-16 16:24:09 -0800

    Display search results

--- carBidderApp/views.py
+++ carBidderApp/views.py
@@ -1,34 +1,96 @@
 from django.shortcuts import render
-from .models import Employee
+from django.db import connection
 
 # Create your views here.
-def testmysql(request):
-    employee = Employee.objects.all()
-    context = {
-        'user_ssn': employee[0].ssn,
-        'user_name': employee[0].lname,
-    }
-    return render(request, 'home.html', context)
 
 
 def searchCar(request):
-    # Car makes, sorted alphabetically
-    makes = ["Audi", "BMW", "Chevrolet", "Ford", "Honda", "Hyundai", "Jeep", "Kia", "Lexus", "Mercedes", "Nissan", "Tesla", "Toyota", "Volkswagen"]
+    # Fetch unique values for dropdowns from the database
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT DISTINCT make FROM LISTED_VEHICLES ORDER BY make")
+        makes = [row[0] for row in cursor.fetchall()]
 
-    # Year options
-    years = [2016, 2017, 2018, 2019, 2020, 2021, 2022]
+        cursor.execute(
+            "SELECT DISTINCT year_of_production FROM LISTED_VEHICLES ORDER BY year_of_production")
+        years = [row[0] for row in cursor.fetchall()]
 
-    # Mileage options (0 to 20000, step by 4000)
-    mileages = list(range(0, 20001, 4000))
+        cursor.execute(
+            "SELECT DISTINCT mileage FROM LISTED_VEHICLES ORDER BY mileage")
+        mileages = [row[0] for row in cursor.fetchall()]
 
-    # Price options (10000 to 35000, step by 5000)
-    prices = list(range(10000, 35001, 5000))
+        cursor.execute(
+            "SELECT DISTINCT price FROM LISTED_VEHICLES ORDER BY price")
+        prices = [row[0] for row in cursor.fetchall()]
 
-    context = {
+    error_message = None
+
+    # Fetch filter parameters
+    make = request.GET.get('make')
+    min_year = request.GET.get('min_year')
+    max_year = request.GET.get('max_year')
+    min_mileage = request.GET.get('min_mile')
+    max_mileage = request.GET.get('max_mile')
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+
+    # Check for invalid filter ranges
+    if min_year and max_year and int(min_year) > int(max_year):
+        error_message = "Minimum year cannot be greater than maximum year."
+    elif min_mileage and max_mileage and int(min_mileage) > int(max_mileage):
+        error_message = "Minimum mileage cannot be greater than maximum mileage."
+    elif min_price and max_price and int(min_price) > int(max_price):
+        error_message = "Minimum price cannot be greater than maximum price."
+
+    # If there's an error, return early with the error message
+    if error_message:
+        return render(request, 'searchCar.html', {'error_message': error_message})
+
+# Initialize the base query
+    query = "SELECT * FROM LISTED_VEHICLES WHERE 1=1"
+
+    # Initialize the parameters list
+    params = []
+
+    # Append conditions to the query based on provided filters
+    if make:
+        query += " AND make = %s"
+        params.append(make)
+
+    if min_year:
+        query += " AND year_of_production >= %s"
+        params.append(min_year)
+
+    if max_year:
+        query += " AND year_of_production <= %s"
+        params.append(max_year)
+
+    if min_mileage:
+        query += " AND mileage >= %s"
+        params.append(min_mileage)
+
+    if max_mileage:
+        query += " AND mileage <= %s"
+        params.append(max_mileage)
+
+    if min_price:
+        query += " AND price >= %s"
+        params.append(min_price)
+
+    if max_price:
+        query += " AND price <= %s"
+        params.append(max_price)
+
+    # Execute the query
+    with connection.cursor() as cursor:
+        cursor.execute(query, params)
+        vehicles = cursor.fetchall()
+
+    return render(request, 'searchCar.html', {
+        'vehicles': vehicles,
         'makes': makes,
         'years': years,
         'mileages': mileages,
-        'prices': prices
-    }
-
-    return render(request, 'searchCar.html', context)
+        'prices': prices,
+        'error_message': error_message
+    })
